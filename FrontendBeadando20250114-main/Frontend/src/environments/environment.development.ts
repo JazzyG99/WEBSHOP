b016b1d22b29ba76c917132f501fe41839973371
JazzyG99 <144050370+JazzyG99@users.noreply.github.com>
2025-01-17 18:00:25 +0100

    Add files via upload

---
+++ FrontendBeadando20250114-main/Frontend/src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production:true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDIAnpSdwirTPhruA3uD97V9o-mYkzhV6Y",
+    authDomain: "webshophazi-29c59.firebaseapp.com",
+    projectId: "webshophazi-29c59",
+    storageBucket: "webshophazi-29c59.firebasestorage.app",
+    messagingSenderId: "648369331177",
+    appId: "1:648369331177:web:88440f0b9c785805918978",
+    measurementId: "G-GR0SP7FENV"
+  }
+}
