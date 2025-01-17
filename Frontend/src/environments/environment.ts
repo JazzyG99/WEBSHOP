b1a8979824857f3d969d9b7b806822bda8ef9888
JazzyG99 <144050370+JazzyG99@users.noreply.github.com>
2025-01-17 17:38:30 +0100

    Add files via upload

---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production:false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCd4Jxw1tvmyixR0LI17OxnjxFJsYD0o8c",
+    authDomain: "frontend-7e3c6.firebaseapp.com",
+    projectId: "frontend-7e3c6",
+    storageBucket: "frontend-7e3c6.firebasestorage.app",
+    messagingSenderId: "558407624225",
+    appId: "1:558407624225:web:68fe8ad63366bc4972e560",
+    measurementId: "G-6T1R0LVNFE"
+  }
+}
